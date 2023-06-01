01ce33a1081b518a31ddf76119e084953d1022c0
Howard Chang <120439150+Howard-Chang327@users.noreply.github.com>
2023-05-31 17:12:21 -0700

    update reward function

--- rlproject/toolkits/reward.py
+++ rlproject/toolkits/reward.py
@@ -2,28 +2,31 @@ import numpy as np
 
 def cal_reward(state, goal):
 
-
+    camera_center = [-0.237, -0.095]
+    
     #extract data from the input param
-    x, y, yaw, xc, yc = state 
+    xrob, yrob, yaw, xc, yc = state 
     # print("state: ", state)
     goal_x, goal_y = goal
     # print("goal: ", goal)
     
-    dist_robot2cube = np.sqrt((x - xc)**2 + (y - yc)**2)
-    dist_cube2goal = np.sqrt((xc - goal_x)**2 + (yc - goal_y)**2)
+    dist_robot2cube = np.sqrt((camera_center[0] - xc)**2 + (camera_center[1] - yc)**2)
+    dist_rob2goal = np.sqrt((goal_x - xrob)**2 + (goal_y - yrob)**2)
     
-    reward = 0
+    reward = 100
     
-    if dist_robot2cube <= 0.2:
-        reward = 1000
+    if dist_robot2cube <= 0.1:
+        reward = 100
     else:
-        reward = - dist_robot2cube * 10
+        reward /= dist_robot2cube
     
-    if dist_cube2goal <= 0.2:
-        reward += 1000
+    if dist_rob2goal <= 0.2:
+        reward *= 100
     else:
-        reward -= dist_cube2goal * 10
-
+        reward /= dist_rob2goal
         
+    if xc and yc == None:
+        reward = -10000
+    
     return reward
     
