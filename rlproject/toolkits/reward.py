b66d0c548abb2a68ec130eae586cd02131b90e19
Howard Chang <120439150+Howard-Chang327@users.noreply.github.com>
2023-06-01 16:30:06 -0700

    update reward.py

--- rlproject/toolkits/reward.py
+++ rlproject/toolkits/reward.py
@@ -3,7 +3,7 @@ import numpy as np
 def cal_reward(state, goal):
     
     corner_pos = [[0.15, 0.28], [0.15, 0.15], [-0.15, 0.15], [-0.15, 0.28]]
-    
+    center_pos = [0, 0.215]
 
     camera_center = [-0.237, -0.095]
     cube_threshold = 0.02
@@ -17,6 +17,7 @@ def cal_reward(state, goal):
 
     dist_robot2cube = np.sqrt((camera_center[0] - xc)**2 + (camera_center[1] - yc)**2)
     dist_robot2goal = np.sqrt((goal_x - xrob)**2 + (goal_y - yrob)**2)
+    dist_robot2center = np.sqrt((center_pos[0] - xrob)**2 + (center_pos[1] - yrob)**2)
     
     # Check if the robot arm is 'touching' the cube
     touching_cube = dist_robot2cube <= cube_threshold
@@ -24,7 +25,7 @@ def cal_reward(state, goal):
     
     # Stage 1: Find the cube
     if loss_the_cube:
-        reward = -1000
+        reward = 1/dist_robot2center * 0.1
         if xrob > 0.14 or xrob < -0.14 or yrob > 0.27 or yrob < 0.16: #if the cube is out of the frame
             reward -= 1000
     # Stage 2: Move to the cube
