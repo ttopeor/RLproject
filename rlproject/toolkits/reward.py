d499d90dfee90abf106214f009851a8b76c851b4
Howard Chang <120439150+Howard-Chang327@users.noreply.github.com>
2023-06-01 12:40:49 -0700

    update reward function

--- rlproject/toolkits/reward.py
+++ rlproject/toolkits/reward.py
@@ -3,38 +3,35 @@ import numpy as np
 def cal_reward(state, goal):
 
     camera_center = [-0.237, -0.095]
+    cube_threshold = 0.02
+    goal_threshold = 0.02
     
     #extract data from the input param
     xrob, yrob, yaw, xc, yc = state 
     # print("state: ", state)
     goal_x, goal_y = goal
     # print("goal: ", goal)
-    
-    if xc == None or yc == None:
-        reward = -10000
-        return reward
 
     dist_robot2cube = np.sqrt((camera_center[0] - xc)**2 + (camera_center[1] - yc)**2)
-    dist_rob2goal = np.sqrt((goal_x - xrob)**2 + (goal_y - yrob)**2)
+    dist_robot2goal = np.sqrt((goal_x - xrob)**2 + (goal_y - yrob)**2)
     
-    weight = 0
-    reward = 0
+    # Check if the robot arm is 'touching' the cube
+    touching_cube = dist_robot2cube <= cube_threshold
     
-    if dist_robot2cube <= 0.02:
-        weight = 1
-    else:
-        weight = -dist_robot2cube
-    
-    if dist_rob2goal <= 0.02:
-        reward = 100
-    else:
-        reward = 1/dist_rob2goal
+    # Stage 1: Move to the cube
+    if not touching_cube:
+        reward = 1/dist_robot2cube
         
-    reward = reward * weight
+    # Stage 2: Move the cube to the goal
+    else:
+        if dist_robot2goal <= goal_threshold:
+            reward = 1000  # Big reward for reaching the goal
+        else:
+            reward = 1/dist_robot2goal
 
-    
-
+    if xc == 10 or yc == 10: #if the cube is out of the camera view
+        reward = -1000
+        
     #print("reward: ", reward)
-
     return reward
     
